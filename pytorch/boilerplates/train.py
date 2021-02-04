343b5376e8da818a8cce1dc107715d744446ab12
shaowen <shaowen310@outlook.com>
2021-02-04 14:53:08 +0000

    fix

--- pytorch/boilerplates/train.py
+++ pytorch/boilerplates/train.py
@@ -26,7 +26,7 @@ def train_one_epoch(epoch, model, dataloader, optimizer, criterion, device, log_
         if batch % log_interval == 0 and batch > 0:
             cur_loss = log_loss / log_interval
             elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d} batches | ms/batch {:5.2f} | '
+            print('| epoch {:3d} | {:5d} batches | {:5.2f} ms/batch | '
                   'loss {:5.2f} | ppl {:8.2f}'.format(epoch, batch, elapsed * 1000 / log_interval,
                                                       cur_loss, math.exp(cur_loss)))
             log_loss = 0
