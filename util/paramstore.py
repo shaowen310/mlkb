152ec5b10197a8d121e441042339cf6a779bf915
shaowen <shaowen310@outlook.com>
2021-03-02 07:22:37 +0000

    paramstore

--- util/paramstore.py
+++ util/paramstore.py
@@ -1,6 +1,16 @@
 import collections
 import os
 import json
+import uuid
+
+
+class KeyGen:
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        print(str(uuid.uuid4())[:8])
+        return str(uuid.uuid4())[:8]
 
 
 class JsonStore(collections.MutableMapping):
@@ -33,8 +43,8 @@ class JsonStore(collections.MutableMapping):
     def __len__(self):
         return len(self.keys())
 
-    def __contains__(self):
-        raise NotImplementedError()
+    def __contains__(self, key):
+        return os.path.exists(self._getfp(key))
 
     def keys(self):
         keyset = set()
@@ -52,3 +62,14 @@ class JsonStore(collections.MutableMapping):
 
     def get(self, key):
         return self[key]
+
+
+class ParamStore(JsonStore):
+    def __init__(self, root_dir):
+        super().__init__(root_dir)
+        self.keygen = KeyGen()
+
+    def add(self, model_name, obj):
+        for key in map(lambda randkey: '_'.join((model_name, randkey)), self.keygen):
+            if key not in self:
+                self[key] = obj
