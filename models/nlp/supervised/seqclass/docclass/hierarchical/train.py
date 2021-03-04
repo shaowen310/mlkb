540d42e37f7c6ea7ddfdfba05edba0e36a337bd2
shaowen <shaowen310@outlook.com>
2021-03-04 17:25:09 +0000

    better record

--- models/nlp/supervised/seqclass/docclass/hierarchical/train.py
+++ models/nlp/supervised/seqclass/docclass/hierarchical/train.py
@@ -13,6 +13,8 @@ from sklearn import metrics
 
 from dataset import HANDataset
 from model import HierAttNet
+import loggingutil
+from paramstore import ParamStore
 
 
 def get_max_lengths(data_path):
@@ -82,42 +84,46 @@ def get_args():
 
 
 def train(opt):
+    opt['saved_path'] = os.path.join(opt['saved_path'], opt['id_'])
+    logger = loggingutil.get_logger(opt['id_'])
     if torch.cuda.is_available():
         torch.cuda.manual_seed(123)
     else:
         torch.manual_seed(123)
-    if not os.path.isdir(opt.saved_path):
-        os.makedirs(opt.saved_path)
-    output_file = open(opt.saved_path + os.sep + "logs.txt", "w")
-    output_file.write("Model's parameters: {}".format(vars(opt)))
-    training_params = {"batch_size": opt.batch_size, "shuffle": True, "drop_last": True}
-    test_params = {"batch_size": opt.batch_size, "shuffle": False, "drop_last": False}
+    if not os.path.isdir(opt['saved_path']):
+        os.makedirs(opt['saved_path'])
+    output_file = open(opt['saved_path'] + os.sep + "logs.txt", "w")
+    output_file.write("Model's parameters: {}".format(opt))
+    training_params = {"batch_size": opt['batch_size'], "shuffle": True, "drop_last": True}
+    test_params = {"batch_size": opt['batch_size'], "shuffle": False, "drop_last": False}
 
-    max_word_length, max_sent_length = get_max_lengths(opt.train_set)
-    training_set = HANDataset(opt.train_set, opt.word2vec_path, max_sent_length, max_word_length)
+    max_word_length, max_sent_length = get_max_lengths(opt['train_set'])
+    training_set = HANDataset(opt['train_set'], opt['word2vec_path'], max_sent_length, max_word_length)
     training_generator = DataLoader(training_set, **training_params)
-    test_set = HANDataset(opt.test_set, opt.word2vec_path, max_sent_length, max_word_length)
+    test_set = HANDataset(opt['test_set'], opt['word2vec_path'], max_sent_length, max_word_length)
     test_generator = DataLoader(test_set, **test_params)
 
-    model = HierAttNet(opt.word_hidden_size, opt.sent_hidden_size, opt.batch_size, training_set.num_classes,
+    model = HierAttNet(opt['word_hidden_size'], opt['sent_hidden_size'], opt['batch_size'], training_set.num_classes,
                        len(training_set.index_to_word), max_sent_length, max_word_length)
 
-    if os.path.isdir(opt.log_path):
-        shutil.rmtree(opt.log_path)
-    os.makedirs(opt.log_path)
-    # writer = SummaryWriter(opt.log_path)
-    # writer.add_graph(model, torch.zeros(opt.batch_size, max_sent_length, max_word_length))
+    if os.path.isdir(opt['log_path']):
+        shutil.rmtree(opt['log_path'])
+    os.makedirs(opt['log_path'])
+    # writer = SummaryWriter(opt['log_path)
+    # writer.add_graph(model, torch.zeros(opt['batch_size, max_sent_length, max_word_length))
 
     if torch.cuda.is_available():
         model.cuda()
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=opt.lr, momentum=opt.momentum)
+    optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()),
+                                lr=opt['lr'],
+                                momentum=opt['momentum'])
     best_loss = 1e5
     best_epoch = 0
     model.train()
     num_iter_per_epoch = len(training_generator)
-    for epoch in range(opt.num_epoches):
+    for epoch in range(opt['num_epoches']):
         for iter, (feature, label) in enumerate(training_generator):
             if torch.cuda.is_available():
                 feature = feature.cuda()
@@ -131,13 +137,13 @@ def train(opt):
             training_metrics = get_evaluation(label.cpu().numpy(),
                                               predictions.cpu().detach().numpy(),
                                               list_metrics=["accuracy"])
-            if not ((iter + 1) % 1000):
-                print("Epoch: {}/{}, Iteration: {}/{}, Lr: {}, Loss: {}, Accuracy: {}".format(
-                    epoch + 1, opt.num_epoches, iter + 1, num_iter_per_epoch, optimizer.param_groups[0]['lr'], loss,
+            if not ((iter + 1) % 100):
+                logger.debug("Epoch: {}/{}, Iteration: {}/{}, Lr: {}, Loss: {}, Accuracy: {}".format(
+                    epoch + 1, opt['num_epoches'], iter + 1, num_iter_per_epoch, optimizer.param_groups[0]['lr'], loss,
                     training_metrics["accuracy"]))
             # writer.add_scalar('Train/Loss', loss, epoch * num_iter_per_epoch + iter)
             # writer.add_scalar('Train/Accuracy', training_metrics["accuracy"], epoch * num_iter_per_epoch + iter)
-        if epoch % opt.test_interval == 0:
+        if epoch % opt['test_interval'] == 0:
             model.eval()
             loss_ls = []
             te_label_ls = []
@@ -159,25 +165,33 @@ def train(opt):
             te_label = np.array(te_label_ls)
             test_metrics = get_evaluation(te_label, te_pred.numpy(), list_metrics=["accuracy", "confusion_matrix"])
             output_file.write("Epoch: {}/{} \nTest loss: {} Test accuracy: {} \nTest confusion matrix: \n{}\n\n".format(
-                epoch + 1, opt.num_epoches, te_loss, test_metrics["accuracy"], test_metrics["confusion_matrix"]))
-            if not ((iter + 1) % 1000):
-                print("Epoch: {}/{}, Lr: {}, Loss: {}, Accuracy: {}".format(epoch + 1, opt.num_epoches,
-                                                                            optimizer.param_groups[0]['lr'], te_loss,
-                                                                            test_metrics["accuracy"]))
+                epoch + 1, opt['num_epoches'], te_loss, test_metrics["accuracy"], test_metrics["confusion_matrix"]))
+
+            logger.info("Epoch: {}/{}, Lr: {}, Loss: {}, Accuracy: {}".format(epoch + 1, opt['num_epoches'],
+                                                                              optimizer.param_groups[0]['lr'], te_loss,
+                                                                              test_metrics["accuracy"]))
             # writer.add_scalar('Test/Loss', te_loss, epoch)
             # writer.add_scalar('Test/Accuracy', test_metrics["accuracy"], epoch)
             model.train()
-            if te_loss + opt.es_min_delta < best_loss:
+            if te_loss + opt['es_min_delta'] < best_loss:
                 best_loss = te_loss
                 best_epoch = epoch
-                torch.save(model, opt.saved_path + os.sep + "whole_model_han")
+                torch.save(model, opt['saved_path'] + os.sep + "whole_model_han")
 
             # Early stopping
-            if epoch - best_epoch > opt.es_patience > 0:
-                print("Stop training at epoch {}. The lowest loss achieved is {}".format(epoch, te_loss))
+            if epoch - best_epoch > opt['es_patience'] > 0:
+                logger.info("Stop training at epoch {}. The lowest loss achieved is {}".format(epoch, te_loss))
                 break
+
+            output_file.close()
 
 
 if __name__ == '__main__':
-    opt = get_args()
+    opt = vars(get_args())
+
+    model_name = 'HAN'
+
+    pstore = ParamStore()
+    pstore.add(model_name, opt)
+
     train(opt)
