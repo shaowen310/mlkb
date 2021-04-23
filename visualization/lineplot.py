7ef1e3473cf040212c236e42949dab6b5451cd76
shaowen <shaowen310@outlook.com>
2021-04-23 05:34:49 +0000

    lineplot

--- visualization/lineplot.py
+++ visualization/lineplot.py
@@ -10,16 +10,21 @@ x = torch.randn(100, 1)
 y = 5 * x + torch.randn(100, 1)
 linearreg = LinearRegression()
 linearreg.fit(x, y)
+h = 0.01
+x_a = np.arange(x.min(), x.max(), h)
+y_pred = linearreg.predict(x_a.reshape(-1, 1)).ravel()
 
 # %%
-h = 0.01
-
 fig, axes = plt.subplots(1, 1)
-x_a = np.arange(x.min(), x.max(), h)
+
+x_pos = np.arange(0, 4, 1)
+y = np.array([0.796, 0.808, 0.809, 0.805])
+
 # line
-axes.plot(x_a, linearreg.predict(x_a.reshape(-1, 1)).ravel())
+axes.plot(x_pos, y)
+plt.xticks(x_pos, ['50', '100', '150', '200'])
 # dots
-axes.scatter(x[:, 0], y[:, 0])
+# axes.scatter(x[:, 0], y[:, 0])
 
 plt.show()
 
