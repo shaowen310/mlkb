9a0fe80afb5dcd688a0f1cd55d8a84d02ba1d6f8
Shaowen <shaowen310@outlook.com>
2021-05-02 19:56:34 +0800

    squad

---
+++ datasets/nlp/qa/squad.py
@@ -0,0 +1,9 @@
+from datasets import load_dataset
+
+dataset = load_dataset("squad")
+
+_URL = "https://rajpurkar.github.io/SQuAD-explorer/dataset/"
+_URLS = {
+    "train": _URL + "train-v1.1.json",
+    "dev": _URL + "dev-v1.1.json",
+}
